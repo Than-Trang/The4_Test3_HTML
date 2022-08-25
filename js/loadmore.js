a6b6769309eaad36778cfc97b199ea6063229821
Than-Trang <83278820+Than-Trang@users.noreply.github.com>
2022-08-25 14:19:11 +0700

    fix

--- js/loadmore.js
+++ js/loadmore.js
@@ -1,9 +1,9 @@
 $(function(){
-    $("tr").slice(0, 10).css("display", "table-row"); // select the first ten
+    $("tr").slice(0, 10).css("display", "table-row").addClass("tb-show").removeClass("tb-hidden"); // select the first ten
     $("#load").click(function(e){ // click event for load more
         e.preventDefault();
-        $("tr:hidden").slice(0, 10).css("display", "table-row"); // select next 10 hidden divs and show them
-        if($("tr:hidden").length == 0){ // check if any hidden divs still exist
+        $(".tb-hidden").slice(0, 10).css("display", "table-row").addClass("tb-show").removeClass("tb-hidden");// select next 10 hidden divs and show them
+        if($(".tb-hidden").length == 0){ // check if any hidden divs still exist
             $("#load").hide();
         }
     });
